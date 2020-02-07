ec391be2d08b0de23441698d9c0bab3a9b7ead7f
krywojciechowski <krywojciechowski50@gmail.com>
2020-02-07 17:05:44 +0100

    'project'

---
+++ search.py
@@ -0,0 +1,24 @@
+
+import requests
+import json
+import sys
+import datetime
+import easygui
+
+
+def main():
+    query = sys.argv[1]
+    print(query)
+    key = 'AIzaSyAErHc2pCqwXSGMtUpyrEv5UcPuNzbDYZU'
+
+    doc = requests.get(f"https://www.googleapis.com/youtube/v3/search?q={query}&key={key}&part=snippet&type=video")
+    res = json.loads(doc.text)
+
+    date = datetime.date.today()
+
+    if res['items'][0]['snippet']['publishedAt'].split('T')[0] == date.strftime('%Y-%m-%d'):
+        easygui.msgbox('New video on  youtube.com/watch?v='+res['items'][0]['id']['videoId'])
+
+
+if __name__ == '__main__':
+    main()
